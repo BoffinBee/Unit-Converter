f329d094d20d0fc2764c154052a16d5354477710
BoffinBee <boffinbee@gmail.com>
2022-08-02 12:11:56 +0100

    Convert button now working!

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-20 meters = 65.616 feet | 20 feet = 6.096 meters
-*/
-
 inputEl = document.getElementById("input-field");
+convertBtnEl = document.getElementById("convert-btn");
+lengthDetailsEl = document.getElementById("length-details");
+volumeDetailsEl = document.getElementById("volume-details");
+massDetailsEl = document.getElementById("mass-details");
+
+function meterToFeet (meter) {
+    return (parseFloat(meter) * 3.281).toFixed(3);
+}
+
+function feetToMeter (feet) {
+    return (parseFloat(feet) / 3.281).toFixed(3);
+}
+
+function literToGallon (liter) {
+    return (parseFloat(liter) * 0.264).toFixed(3);
+}
+
+function gallonToLiter (gallon) {
+    return (parseFloat(gallon) / 0.264).toFixed(3);
+}
+
+function kilogramToPound (kilogram) {
+    return (parseFloat(kilogram) * 2.204).toFixed(3);
+}
+
+function poundToKilogram (pound) {
+    return (parseFloat(pound) / 2.204).toFixed(3);
+}
+
+function renderDetails() {
+    const inputValue = inputEl.value;
+    lengthDetailsEl.textContent = `${inputValue} meters = ${meterToFeet(inputValue)} feet | ${inputValue} feet = ${feetToMeter(inputValue)} meter`;
+    volumeDetailsEl.textContent = `${inputValue} liters = ${literToGallon(inputValue)} gallons | ${inputValue} gallons = ${gallonToLiter(inputValue)} liters`;
+    massDetailsEl.textContent = `${inputValue} kilograms = ${kilogramToPound(inputValue)} pounds | ${inputValue} pounds = ${poundToKilogram(inputValue)} kilograms`;
+
+}
+
+convertBtnEl.addEventListener("click", renderDetails);
